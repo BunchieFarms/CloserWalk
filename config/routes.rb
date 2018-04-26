30db07c3c059cd55378d3113e334e3ce364e602b
BunchieFarms <bohmer@uncc.edu>
2018-04-26 01:29:15 -0400

    started work on landing page header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
