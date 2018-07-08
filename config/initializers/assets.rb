15d08fffc46b8f20727ab0d193cd5a1acbb94f20
BunchieFarms <bohmer@uncc.edu>
2018-07-08 19:44:44 -0400

    Mostly finished with about page, moving on

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( welcome.css )
+Rails.application.config.assets.precompile += %w( about.css )
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.intialize_on_precompile = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get '/index' => 'welcome#index'
   root 'welcome#index'
+  get '/about' => 'welcome#about'
 
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
